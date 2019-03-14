f3ba925fac78375546f933cfc105ce1caccb4dac
wai123999 <tommy_6699045@hotmail.com>
2019-03-14 17:33:42 +0800

    testing branch

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ var app = express();
 
 var obj ;
 fs.readFile("staff.txt",{encoding:"utf-8"},function(err,data){
-	console.log(data);
+	//console.log(data);
 	//read utf8 something error,need cut the position 0
 	data = data.slice(1);
 	obj = JSON.parse(data);
-	console.log(obj);
+	console.log(obj.length);
+	//console.log(obj);
 });
 
 
@@ -28,12 +29,16 @@ app.get('/',function(req,res){
 	res.render('main');
 });
 
+app.get('/getstaff',function(req,res){
+	 res.json(obj);
+});
+
 app.get('/win',function(req,res){
 	fs.writeFile('中獎名單.txt',req.query.winnerDept +"-" + req.query.winnerName + "\r\n",{flag:"a+"},function(err){
 		if ( err ){
 			return console.log(err);
 		}
   		console.log("Winner is " + req.query.winnerDept +"-" + req.query.winnerName+ " save..\r\n");
-	
+
 	});
 });
--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,25 @@
-var deptArr = ["IT","KA","Finance","ASO","運輸","OK"];
+var deptArr = ["IT","KA","Finance","ASO","Sales","OK"];
 var staffArr = ["john","kin","terry","建華","lam_sir","朱經理"];
+var deptObjArr = [];
 var str = "";
 var nameWho;
 var deptWho;
-
+var jsonObj;
+var deptIndex;
+$.ajax({
+    url: "/getstaff",
+    type: "get", //send it through get method
+    success: function(response) {
+    //Do Something
+      console.log("Receive Json Obj..");
+      classificationByDept(response);
+      console.log("classification finish..");
+      console.log(deptObjArr);
+    },
+    error: function(xhr) {
+      //Do Something to handle error
+    }
+  });
 var unknownStr = "???????";
 var $lottery_winner_dept = $("#lottery-winner-dept");
 var $lottery_winner_name = $("#lottery-winner-name");
@@ -15,12 +31,13 @@ var $start_btn = $("#start_btn");
 var deptWidth = $deptWrapper.css("width");
 var nameWidth = $nameWrapper.css("width");
 
+
  $lottery_winner_dept.html(returnSliderStr(unknownStr,1));
  $lottery_winner_name.html(returnSliderStr(unknownStr,1));
 
    $start_btn.click(function(){
       shuffle(deptArr);
-      shuffle(staffArr);
+      shuffle(deptArr);
     //mixName();
         $winner_dept.html(returnSliderStr(deptArr));
       //animate start
@@ -37,7 +54,13 @@ var nameWidth = $nameWrapper.css("width");
             console.log("The Dept is :" + deptArr[deptWho]);
             //staffArray.splice(who,1);
             //console.log(staffArray);
-            $winner_name.html(returnSliderStr(staffArr));
+            //$winner_name.html(returnSliderStr(staffArr));
+            deptIndex = findDeptArrObjIndex(deptArr[deptWho]);
+
+            $winner_name[0].style.setProperty('--name-count', deptObjArr[deptIndex].staffArr.length);
+            //不要用一句過...用attribute去控制
+            $winner_name.html(returnStaffSliderStr(deptObjArr[deptIndex]));
+
             $winner_name.addClass("nameGlitch");
             //str = "";
             //mixName();
@@ -49,28 +72,31 @@ var nameWidth = $nameWrapper.css("width");
         $winner_name.removeClass("nameGlitch");
         $winner_name.css("transform","translateX(" + currTrans + "px)");
         staffWho = parseInt(Math.abs(currTrans) / parseInt((nameWidth).substring(0,(nameWidth).indexOf('p'))));
-        console.log("The Staff is :" + staffArr[staffWho]);
-
+        console.log("The Staff is :" + deptObjArr[deptIndex].staffArr[staffWho]);
+        shuffle(deptObjArr[deptIndex].staffArr);
         //staffArr.splice(staffWho,-1);
         //send a request to node ...
         $.ajax({
             url: "/win",
             type: "get", //send it through get method
             data: {
-                winnerDept : deptArr[deptWho],
-                winnerName : staffArr[staffWho]
+                winnerDept : deptObjArr[deptIndex].deptName,
+                winnerName : deptObjArr[deptIndex].staffArr[staffWho]
             },
             success: function(response) {
-            //Do Something
+
             },
             error: function(xhr) {
               //Do Something to handle error
-            }
+            },
+            async:true
           });
       },6000);
       setTimeout(function(){
-        $lottery_winner_dept.html(returnSliderStr(unknownStr,1));
-        $lottery_winner_name.html(returnSliderStr(unknownStr,1));
+        //$lottery_winner_dept.html(returnSliderStr(unknownStr,1));
+        //when rehook dom .. the css will dispear
+      //  $("#lottery-winner-dept").find("span").eq(deptWho).html(unknownStr);
+      //  $("#lottery-winner-name").find("span").eq(staffWho).html(unknownStr);
       },7500);
    });
 
@@ -109,4 +135,45 @@ function returnSliderStr(arr,mode)
      }
  }
    return sliderStr;
+}
+function returnStaffSliderStr(obj)
+{
+  var sliderStr = "";
+  for ( var k = 0; k < obj.staffArr.length ; k++)
+    sliderStr += "<span>"+ obj.staffArr[k] +"</span>";
+  return sliderStr;
+}
+function findDeptArrObjIndex(deptName)
+{
+  for ( var k = 0 ; k < deptObjArr.length ; k++)
+  {
+    if ( deptObjArr[k].deptName == deptName)
+       return k;
+  }
+  return -1;
+}
+
+function Dept(deptName){
+    this.staffArr = [];
+    this.deptName = deptName;
+}
+
+Dept.prototype.addStaff = function(staffName){
+  this.staffArr.push(staffName);
+};
+Dept.prototype.getName = function() { return this.deptName;};
+
+
+function classificationByDept(jsonObj){
+    for ( let j = 0 ; j < deptArr.length ; j++)
+    {
+      deptObjArr[j] = new Dept(deptArr[j]);
+      for ( let  i = 0 ; i < jsonObj.length ; i++)
+      {
+         if ( deptObjArr[j].getName() == jsonObj[i].dept )
+         {
+             deptObjArr[j].addStaff(jsonObj[i].name);
+         }
+      }
+    }
 }
