4de6d11638fd6b9e0a33325b13f5db5af1372f28
wai123999 <tommy_6699045@hotmail.com>
2019-03-14 14:55:10 +0800

    完成動畫功能

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var app = express();
 var obj ;
 fs.readFile("staff.txt",{encoding:"utf-8"},function(err,data){
 	console.log(data);
+	//read utf8 something error,need cut the position 0
 	data = data.slice(1);
 	obj = JSON.parse(data);
 	console.log(obj);
@@ -25,4 +26,14 @@ app.set('view engine','ejs');
 
 app.get('/',function(req,res){
 	res.render('main');
+});
+
+app.get('/win',function(req,res){
+	fs.writeFile('中獎名單.txt',req.query.winnerDept +"-" + req.query.winnerName + "\r\n",{flag:"a+"},function(err){
+		if ( err ){
+			return console.log(err);
+		}
+  		console.log("Winner is " + req.query.winnerDept +"-" + req.query.winnerName+ " save..\r\n");
+	
+	});
 });
--- public/js/index.js
+++ public/js/index.js
@@ -1,30 +1,77 @@
-var SPAN_LEN = 20;
-var deptArr = ["IT","KA","Finance","ASO","運輸","OK"];      
+var deptArr = ["IT","KA","Finance","ASO","運輸","OK"];
+var staffArr = ["john","kin","terry","建華","lam_sir","朱經理"];
 var str = "";
+var nameWho;
+var deptWho;
 
-   
-   $("#start_btn").click(function(){
+var unknownStr = "???????";
+var $lottery_winner_dept = $("#lottery-winner-dept");
+var $lottery_winner_name = $("#lottery-winner-name");
+var $deptWrapper = $(".lottery-winner:nth-child(1)");
+var $nameWrapper = $(".lottery-winner:nth-child(2)");
+var $winner_dept = $("#lottery-winner-dept");
+var $winner_name = $("#lottery-winner-name");
+var $start_btn = $("#start_btn");
+var deptWidth = $deptWrapper.css("width");
+var nameWidth = $nameWrapper.css("width");
+
+ $lottery_winner_dept.html(returnSliderStr(unknownStr,1));
+ $lottery_winner_name.html(returnSliderStr(unknownStr,1));
+
+   $start_btn.click(function(){
       shuffle(deptArr);
+      shuffle(staffArr);
     //mixName();
-       $("#lottery-winner").html(returnSliderStr(deptArr));
+        $winner_dept.html(returnSliderStr(deptArr));
       //animate start
-       $("#lottery-winner").addClass("glitch");
+        $winner_dept.addClass("glitch");
 
        //timeout , delete lottery-winner and show
        setTimeout(function()
         {
-            var currTrans = $('#lottery-winner').css('transform').split(/[()]/)[1].split(',')[4];
+            var currTrans =  $winner_dept.css('transform').split(/[()]/)[1].split(',')[4];
             console.log(currTrans);
-            $("#lottery-winner").removeClass("glitch");
-            $("#lottery-winner").css("transform","translateX(" + currTrans + "px)");
-            var who =  Math.abs(currTrans) / 120;
-            console.log("The Dept is :" + deptArr[who]);
+            $winner_dept.removeClass("glitch");
+            $winner_dept.css("transform","translateX(" + currTrans + "px)");
+            deptWho = parseInt(Math.abs(currTrans) / parseInt((deptWidth).substring(0,(deptWidth).indexOf('p'))));
+            console.log("The Dept is :" + deptArr[deptWho]);
             //staffArray.splice(who,1);
             //console.log(staffArray);
-        
+            $winner_name.html(returnSliderStr(staffArr));
+            $winner_name.addClass("nameGlitch");
             //str = "";
             //mixName();
-        },6000);
+        },3000);
+      setTimeout(function(){
+
+        var currTrans =  $winner_name.css('transform').split(/[()]/)[1].split(',')[4];
+        console.log(currTrans);
+        $winner_name.removeClass("nameGlitch");
+        $winner_name.css("transform","translateX(" + currTrans + "px)");
+        staffWho = parseInt(Math.abs(currTrans) / parseInt((nameWidth).substring(0,(nameWidth).indexOf('p'))));
+        console.log("The Staff is :" + staffArr[staffWho]);
+
+        //staffArr.splice(staffWho,-1);
+        //send a request to node ...
+        $.ajax({
+            url: "/win",
+            type: "get", //send it through get method
+            data: {
+                winnerDept : deptArr[deptWho],
+                winnerName : staffArr[staffWho]
+            },
+            success: function(response) {
+            //Do Something
+            },
+            error: function(xhr) {
+              //Do Something to handle error
+            }
+          });
+      },6000);
+      setTimeout(function(){
+        $lottery_winner_dept.html(returnSliderStr(unknownStr,1));
+        $lottery_winner_name.html(returnSliderStr(unknownStr,1));
+      },7500);
    });
 
    function shuffle(array) {
@@ -46,23 +93,20 @@ var str = "";
 
     return array;
    }
-function returnSliderStr(arr)
+function returnSliderStr(arr,mode)
 {
-   var sliderStr = "";
-   for ( var i = 0; i < arr.length ; i++)
+  //mode = 1 is a str .. 0 is array
+  var sliderStr = "";
+  if ( mode == 1)
+  {
+       sliderStr += "<span>"+arr+"</span>";
+  }
+   else
    {
-       sliderStr += "<span>"+fillTheSpace(arr[i])+"</span>";  
-   }
+     for ( var i = 0; i < arr.length ; i++)
+     {
+       sliderStr += "<span>"+arr[i]+"</span>";
+     }
+ }
    return sliderStr;
-}
-
-function fillTheSpace(str)
-{
-    /*把空格填到str後..直到*/
-    var len = str.length;
-    if ( len < SPAN_LEN ) {
-         for ( var j = 0; j < SPAN_LEN - len ; j++ ) 
-            str += ' ';
-    }
-    return str;
 }
